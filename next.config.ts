dbca82e78fdd63b5a1dbd67e15e0ecd71aa48948
alphafqeer <soulsparkdev01@gmail.com>
2026-02-06 19:15:33 +0300

    Clean next config (remove env injection)

--- next.config.ts
+++ next.config.ts
@@ -7,19 +7,12 @@ const nextConfig: NextConfig = {
     "*.replit.dev",
     "*.sisko.replit.dev",
   ],
+
   images: {
     unoptimized: true,
   },
+
   trailingSlash: true,
-  env: {
-    FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
-    FIREBASE_AUTH_DOMAIN: process.env.FIREBASE_AUTH_DOMAIN,
-    FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID,
-    FIREBASE_STORAGE_BUCKET: process.env.FIREBASE_STORAGE_BUCKET,
-    FIREBASE_MESSAGING_SENDER_ID: process.env.FIREBASE_MESSAGING_SENDER_ID,
-    FIREBASE_APP_ID: process.env.FIREBASE_APP_ID,
-    NEXT_PUBLIC_TESTING_MODE: process.env.NEXT_PUBLIC_TESTING_MODE,
-  },
 };
 
 export default nextConfig;
