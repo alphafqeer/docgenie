6163795894fd7b38e2a457f22abf8a5a582a69d7
alphafqeer <soulsparkdev01@gmail.com>
2026-02-06 21:02:24 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  allowedDevOrigins: [
-    "localhost",
-    "127.0.0.1",
-    "*.replit.dev",
-    "*.sisko.replit.dev",
-  ],
-  images: { unoptimized: true },
+  images: {
+    unoptimized: true,
+  },
   trailingSlash: true,
 };
 
